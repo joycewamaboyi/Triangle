9005168a6f3fcff14399f59a2ea556e345ab150b
tuyjoy92@yahoo.fr <tuyjoy92@yahoo.fr>
2018-12-14 12:09:02 +0200

    add content in javascript

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function mytriangle(){
+    var part  = [
+               parseInt(document.getElementById("part1").value),
+               parseInt(document.getElementById("part2").value),
+               parseInt(document.getElementById("part3").value)
+               ];
+               
+     
+     
